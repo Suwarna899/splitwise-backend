27794535696d7c4ddbc9b41a594adff3a47f6e32
S SUWARNA <ssuwarna@Ss-MacBook-Air.local>
2026-01-10 11:54:59 +0530

    Finalizing backend for Vercel deployment

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const app = require("./src/app");
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
